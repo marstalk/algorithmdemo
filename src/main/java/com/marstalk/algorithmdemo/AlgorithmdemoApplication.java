6f38adb4745cba61d8d9d046434adf011fb370b5
louisl <louisl@synnex.com>
2018-05-23 09:48:21 +0800

    add project

---
+++ src/main/java/com/marstalk/algorithmdemo/AlgorithmdemoApplication.java
@@ -0,0 +1,12 @@
+package com.marstalk.algorithmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmdemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/marstalk/algorithmdemo/graph/Question1.java
@@ -0,0 +1,11 @@
+package com.marstalk.algorithmdemo.graph;
+
+/**
+ * given a string without repeatable element,
+ * find out all sorted string.
+ *
+ *
+ */
+public class Question1 {
+    
+}
