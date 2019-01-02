7221b43d0c955591ab1e970deb76816320e20414
Louisl <louisl@synnex.com>
2019-01-02 09:46:36 +0800

    constant folding

---
+++ src/main/java/com/marstalk/basic/string/ConstantFoldingDemo.java
@@ -0,0 +1,16 @@
+package com.marstalk.basic.string;
+
+public class ConstantFoldingDemo {
+    public static final String abc = "abc";
+    private String name;
+
+    public static void main(String[] args) {
+        String a = "a";
+        String otherA = a;
+        System.out.println(a == "a");//true
+        System.out.println(a == otherA);
+
+        final String b = "b";
+        System.out.println(b == "b");//true
+    }
+}
