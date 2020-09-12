3bdbb0028ca45fab347890afcd260b728a8e2102
marstalk <jiacheng524@live.cn>
2020-09-12 22:27:36 +0800

    uuid

---
+++ src/main/java/com/marstalk/id/UUIDDemo.java
@@ -0,0 +1,15 @@
+package com.marstalk.id;
+
+import java.util.UUID;
+
+/**
+ * 128bit=16byte
+ */
+public class UUIDDemo {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+        //4672acdb-e337-4c03-8d12-604a069ecdd6
+
+    }
+}
