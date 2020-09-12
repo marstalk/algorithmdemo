2aed4acdcfe63878dc83ca448f27e2eec7fab5e9
marstalk <jiacheng524@live.cn>
2020-09-12 22:27:48 +0800

    hashMap demo

---
+++ src/main/java/com/marstalk/jdk8/hashmap/HashMapDemo.java
@@ -0,0 +1,19 @@
+package com.marstalk.jdk8.hashmap;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试JDK8下的hashMap
+ */
+public class HashMapDemo {
+
+    @Test
+    public void initialSize() {
+        Map<String, String> map = new HashMap<>();
+
+
+    }
+}
