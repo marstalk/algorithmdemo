6c4193d8363f3ad750b4ea50d587b76ecc99cca9
marstalk <jiacheng524@live.cn>
2021-01-08 21:04:53 +0800

    solution3 for find a pair that equals to target X

--- src/main/java/com/marstalk/algorithmdemo/infoq/array/Question25.java
+++ src/main/java/com/marstalk/algorithmdemo/infoq/array/Question25.java
@@ -1,6 +1,8 @@
 package com.marstalk.algorithmdemo.infoq.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 查找数组中和等于给定数字的所有元素对
@@ -8,9 +10,31 @@ import java.util.Arrays;
 public class Question25 {
     public static void main(String[] args) {
         int array[] = {-40, -5, 1, 3, 6, 7, 8, 20};
-        findPairsWithSumEqualsToXBruteForce(array, 15);
-        findPairsEqualsToX(array, 15);
-//        findPairsEqualsToXUsingHashing(array, 15);
+//        findPairsWithSumEqualsToXBruteForce(array, 15);
+//        findPairsEqualsToX(array, 15);
+        findPairsEqualsToXUsingHashing(array, 15);
+    }
+
+    /**
+     * 全部放到hashMap中，key=element， value=index，为了避免使用同一个元素两次
+     * 空间复杂度是N
+     * 时间复杂度是N
+     * @param array
+     * @param target
+     */
+    private static void findPairsEqualsToXUsingHashing(int[] array, int target) {
+        Map<Integer, Integer> hash = new HashMap<>(array.length * 3/4);
+        for (int i = 0; i < array.length; i++) {
+            hash.put(array[i], array[i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (hash.get(target - array[i]) != null && hash.get(target - array[i]) != i) {
+                //将已经配对的数据从hash中删除。
+                hash.remove(array[i]);
+                hash.remove(target - array[i]);
+                System.out.println(array[i] + " " + (target - array[i]));
+            }
+        }
     }
 
     private static void findPairsEqualsToX(int[] array, int target) {
@@ -27,9 +51,9 @@ public class Question25 {
                 System.out.println(array[l] + " " + array[r]);
                 l++;
                 r--;
-            }else if (sum < target) {
+            } else if (sum < target) {
                 l++;
-            }else{
+            } else {
                 r--;
             }
         }
