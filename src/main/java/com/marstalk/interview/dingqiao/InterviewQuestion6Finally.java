bb790ceb55027b0023ac88a9023b2682cdfa2663
Louisl <louisl@synnex.com>
2021-01-20 16:38:04 +0800

    finally

--- src/main/java/com/marstalk/interview/dingqiao/InterviewQuestion6Finally.java
+++ src/main/java/com/marstalk/interview/dingqiao/InterviewQuestion6Finally.java
@@ -10,31 +10,24 @@ public class InterviewQuestion6Finally {
         }
     }
 
+    /**
+     * 27 getstatic #3 <java/lang/System.out>
+     * 30 ldc #8 <return ..>
+     * 32 invokevirtual #7 <java/io/PrintStream.println>
+     *
+     * @return
+     */
     private static int test() {
-        // 0 iconst_0
-        // 1 istore_0
         int count = 0;
         try {
-            //  2 aconst_null
-            //  3 astore_1
             String x = null;
-
-            //System.out.println(x.toString() + " ");
+            System.out.println(x.toString() + " ");
         } catch (Exception e) {
-            //System.out.println("catch.. ");
             return --count;
         } finally {
-            //finally一定会执行 except from System.exit and infinite loops
-            // 4 iinc 0 by 1
-            // goto 27 (+20)
             ++count;
-            //System.out.println("finally.. count=" + count);
-        }
 
-        //27 getstatic #3 <java/lang/System.out>
-        //30 ldc #8 <return ..>
-        //32 invokevirtual #7 <java/io/PrintStream.println>
-        System.out.println("return ..");
+        }
         return count;
     }
 }
